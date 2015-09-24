f14fa906a10a93296b706802e248a12b28d50af6
eoglethorpe <ewanogle@gmail.com>
2015-09-24 11:16:13 +0000

    inital commit run and ssettings

--- user_creation_api/run.py
+++ user_creation_api/run.py
@@ -1,5 +1,14 @@
 from eve import Eve
-app = Eve()
+from eve.auth import BasicAuth
+from flask import g
+import os
+
+class MyBasicAuth(BasicAuth):
+    def check_auth(self, username, password, allowed_roles, resource,
+                   method):
+        return username == os.environ['mu'] and password == os.environ['mp']
+
+app = Eve(auth=MyBasicAuth)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
--- user_creation_api/settings.py
+++ user_creation_api/settings.py
@@ -1,11 +1,10 @@
 import os
 
-DOMAIN = {'people': {}}
 MONGO_HOST = 'localhost'
-MONGO_PORT = 27017
+MONGO_PORT = 27017 
 MONGO_USERNAME = os.environ['mu'] 
 MONGO_PASSWORD = os.environ['mp']
-MONGO_DBNAME = 'umm_api'
+MONGO_DBNAME = 'ummapi'
 
 # Enable reads (GET), inserts (POST) and DELETE for resources/collections
 # (if you omit this line, the API will default to ['GET'] and provide
@@ -15,3 +14,48 @@ RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
 # Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
 # individual items  (defaults to read-only item access).
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+ag_request = {
+    # Schema definition, based on Cerberus grammar. Check the Cerberus project
+    # (https://github.com/nicolaiarocci/cerberus) for details.
+    'additional_lookup': { 'url': 'regex("[\w]+")', 'field': 'name' },
+    'schema': {
+       'name': {
+            'type': 'string',
+            'minlength': 1,
+            'maxlength': 100,
+            'required': True,
+            'unique': True,
+        },
+        'email': {
+            'type': 'string',
+            'minlength': 1,
+            'maxlength': 100,
+            'required': True,
+            'unique': True,
+        },
+        'agency_name': {
+            'type': 'string',
+            'minlength': 1,
+            'maxlength': 100,
+            'required': True,
+            'unique': True,
+        },
+        'designation': {
+            'type': 'string',
+            'minlength': 1,
+            'maxlength': 100,
+            'required': True,
+            'unique': True,
+        },
+        'deployment': {
+            'type': 'string',
+            'minlength': 1,
+            'maxlength': 100,
+            'required': True,
+            'unique': True,
+        },
+    }   
+}
+
+DOMAIN = {'ag_request' : ag_request,}
